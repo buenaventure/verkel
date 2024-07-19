5e625fb1dbdf0ff5491de6936915440710343356
Marc Clement <mapacle@mailbox.org>
2024-07-19 09:20:59 +0200

    Show more information about diets

--- app/models/diet.rb
+++ app/models/diet.rb
@@ -3,9 +3,24 @@ class Diet < ApplicationRecord
   has_many :diet_participants
   has_many :participants, through: :diet_participants
 
+  has_many :positive_diet_ingredients, dependent: :restrict_with_error
+  has_many :negative_diet_ingredients, dependent: :restrict_with_error
+
   validates :name, presence: true, uniqueness: true
 
   def to_s
     name
+  end
+
+  def positive_recipes
+    Recipe.where(id:
+      RecipeIngredient.where(id:
+        positive_diet_ingredients.select(:recipe_ingredient_id)).select(:recipe_id))
+  end
+
+  def negative_recipes
+    Recipe.where(id:
+      RecipeIngredient.where(id:
+        negative_diet_ingredients.select(:recipe_ingredient_id)).select(:recipe_id))
   end
 end
